ac5972dd6742822e324499fca8e7bf1166abaee1
sifanurfa <assyka09@gmail.com>
2025-12-03 23:43:11 +0700

    add needed file

--- src/lib/api/index.ts
+++ src/lib/api/index.ts
@@ -1,3 +1,7 @@
 export * from "./interactive/game";
 export * from "./interactive/announcement";
 export * from "./interactive/showreel";
+export * from "./film/announcement";
+export * from "./film/film";
+export * from "./film/video";
+export * from "./film/showreel";
