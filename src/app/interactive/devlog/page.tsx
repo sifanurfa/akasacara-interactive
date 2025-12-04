7ce841ba10b5784b70072e459d888f0ab7d04eb9
sifanurfa <assyka09@gmail.com>
2025-12-04 10:34:12 +0700

    fix: add footer

--- src/app/interactive/devlog/page.tsx
+++ src/app/interactive/devlog/page.tsx
@@ -3,15 +3,20 @@ import React from 'react';
 import Footer from '@/components/Footer';
 import HighlightSection from './HighlightSection';
 import FeaturedSection from './FeaturedSection';
+import Navbar from '@/components/Navbar';
 
 const Devlog = () => {
     return (
+        <>
+        <Navbar />
         <div className="relative bg-black px-container flex flex-col justify-start items-start gap-[23px] overflow-hidden">
             <HighlightSection/>
             <FeaturedSection/>
             <DevlogSection/>
             <Footer/>
         </div>
+        <Footer/>
+        </>
     );
 };
 
